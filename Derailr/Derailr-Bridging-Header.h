7c0ff3e67143934e3a59f3c6b984ad5c6e4d8061
Jamie White <jamie@jgwhite.co.uk>
2014-06-26 14:17:58 +0100

    Add iOS app

---
+++ Derailr/Derailr-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Derailer-Bridging-Header.h
+//  Derailr
+//
+//  Created by Jamie White on 21/06/2014.
+//  Copyright (c) 2014 Jamie White. All rights reserved.
+//
+
+#import <DPFlickView/DPFlickView.h>
